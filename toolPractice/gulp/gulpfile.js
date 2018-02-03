63e96804662a9288314841c3b101674f75435e59
drzz <skyofsmith@163.com>
2018-02-03 10:48:45 +0800

    code

--- toolPractice/gulp/gulpfile.js
+++ toolPractice/gulp/gulpfile.js
@@ -1,6 +1,18 @@
 var gulp = require('gulp');
-var shelljs = require('shelljs');
+// var shelljs = require('shelljs');
+var fs = require('fs');
+var sequence = require('run-sequence');
+var browserify = require('browserify');
 gulp.task('default', function () {
     console.log('default task');
-    shelljs.exec('browserify js/index.js -o js/main.js')
+    sequence('mainjs')
+    // shelljs.exec('browserify js/index.js -o js/main.js')
+});
+
+gulp.task('mainjs', function () {
+    browserify().add('src/js/index.js').bundle().pipe(fs.createWriteStream('js/main.js'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch();
 });
