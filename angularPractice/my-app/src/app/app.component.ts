4611ab6eb8339452834227082f7d81109b01700d
drzz <skyofsmith@163.com>
2018-02-27 00:00:28 +0800

    small edit

--- angularPractice/my-app/src/app/app.component.ts
+++ angularPractice/my-app/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'my app!\n this is first time to use angular cli';
 }
