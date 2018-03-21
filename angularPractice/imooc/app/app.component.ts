ab1271e50114015f9dcfc5dece40e857e79fb173
drzz <skyofsmith@163.com>
2018-03-21 23:22:19 +0800

    angular2 imooc demo

---
+++ angularPractice/imooc/app/app.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app.component.html'
+})
+
+export class AppComponent implements OnInit {
+    private greeting: string;
+    constructor(){
+
+    }
+
+    ngOnInit(){
+        this.greeting = 'angular2';
+    }
+}
