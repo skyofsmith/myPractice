d6f879f7bc01f609bb015c300ee81639d9c7e26e
drzz <skyofsmith@163.com>
2018-03-22 23:34:42 +0800

    ng serve ng generate

--- angularPractice/demo1/src/app/app.module.ts
+++ angularPractice/demo1/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
