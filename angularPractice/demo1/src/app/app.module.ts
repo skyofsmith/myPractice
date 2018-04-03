42b7f006f925f3df321d7a0e4e98f81d5ba0fcb1
drzz <skyofsmith@163.com>
2018-04-03 22:12:34 +0800

    code

--- angularPractice/demo1/src/app/app.module.ts
+++ angularPractice/demo1/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,6 +14,7 @@ import { TestComponent } from './test/test.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
