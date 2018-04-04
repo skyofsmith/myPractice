3a2839a934a10a84fe174b8524577b970e243f1d
drzz <skyofsmith@163.com>
2018-04-04 23:45:27 +0800

    demo1

--- angularPractice/demo1/src/app/user-list/user-list.component.ts
+++ angularPractice/demo1/src/app/user-list/user-list.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  names: string[];
+
+  constructor() {
+    this.names = ['sam', 'tom', 'drzz'];
+  }
 
   ngOnInit() {
   }
