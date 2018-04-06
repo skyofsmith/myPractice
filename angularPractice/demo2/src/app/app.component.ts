9e7d9ebd84030064a3354154a4b3867ed82b8016
drzz <skyofsmith@163.com>
2018-04-06 23:34:19 +0800

    code

--- angularPractice/demo2/src/app/app.component.ts
+++ angularPractice/demo2/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', '../assets/semantic/semantic.css']
 })
 export class AppComponent {
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
