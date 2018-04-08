c4214a21a6143c7a8c0a84ebf8f512214c9ed35d
drzz <skyofsmith@163.com>
2018-04-08 23:58:07 +0800

    01.ts

--- typescriptPractice/01.ts
+++ typescriptPractice/01.ts
@@ -24,3 +24,39 @@ p.greet();
 
 p.age = 6;
 p.ageInYears(12);
+
+class Report {
+
+  data: Array<string>;
+
+  constructor(data: Array<string>) {
+    this.data = data;
+  }
+
+  run() {
+    this.data.forEach(function(line) { console.log(line); });
+  }
+
+}
+
+var r: Report = new Report(['First line', 'Second line']);
+r.run();
+
+class TabbedReport extends Report {
+  headers: Array<string>;
+
+  constructor(headers: string[], values: string[]) {
+    super(values)
+    this.headers = headers;
+  }
+
+  run() {
+    console.log(this.headers);
+    super.run();
+  }
+}
+
+var headers: string[] = ['Name'];
+var data: string[] = ['Alice Green', 'Paul Pfifer', 'Louis Blakenship'];
+var h: TabbedReport = new TabbedReport(headers, data)
+h.run();
