f2532391c0be9c757a40d613cdaf375ba0e38efd
drzz <skyofsmith@163.com>
2018-04-26 00:00:35 +0800

    form-builder

--- angularPractice/demo5/src/app/app.module.ts
+++ angularPractice/demo5/src/app/app.module.ts
@@ -6,11 +6,13 @@ import {
 } from '@angular/forms';
 import { FormsDemoApp } from './app.component';
 import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
+import { DemoFormSkuWithBuilderComponent } from './demo-form-sku-with-builder/demo-form-sku-with-builder.component';
 
 @NgModule({
   declarations: [
     FormsDemoApp,
-    DemoFormSkuComponent
+    DemoFormSkuComponent,
+    DemoFormSkuWithBuilderComponent
   ],
   imports: [
     BrowserModule,
