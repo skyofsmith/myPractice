a9685ed1f659469ac9b4fe6bc449568d09154e93
drzz <skyofsmith@163.com>
2018-06-17 22:39:50 +0800

    element-ui start

--- element-ui/quick-start/main.js
+++ element-ui/quick-start/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import App from './App.vue';
+import Element from 'element-ui';
+Vue.use(Element, {
+    size: 'small',
+    zIndex: 3000
+});
 
-Vue.use(ElementUI);
-
+Vue.use(Element, {
+    size: 'small',
+    zIndex: 3000
+});
 new Vue({
     el: '#app',
     render: h => h(App)
