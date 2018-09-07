6620f8ed0cd0694dcaab349cb71b57b772f5b9d0
drzz <skyofsmith@163.com>
2018-09-07 22:30:31 +0800

    06 webpack

--- toolPractice/webpack/runoob/06/webpack.config.js
+++ toolPractice/webpack/runoob/06/webpack.config.js
@@ -1,17 +1,13 @@
-var webpack=require(webpack);
- 
+var webpack = require(webpack);
+
 module.exports = {
-    entry: "./runoob1.js",
+    entry: './runoob1.js',
     output: {
         path: __dirname,
-        filename: "bundle.js"
+        filename: 'bundle.js'
     },
     module: {
-        loaders: [
-            { test: /\.css$/, loader: "style-loader!css-loader" }
-        ]
+        loaders: [{ test: /\.css$/, loader: 'style-loader!css-loader' }]
     },
-    plugins:[
-    new webpack.BannerPlugin('菜鸟教程 webpack 实例')
-    ]
+    plugins: [new webpack.BannerPlugin('菜鸟教程 webpack 实例')]
 };
