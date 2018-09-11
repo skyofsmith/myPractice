70207c6db8f9181e1a4239d55959c0dc31af4825
drzz <skyofsmith@163.com>
2018-09-11 21:59:38 +0800

    1.ts

---
+++ typescriptPractice/runoob/1.js
@@ -0,0 +1,4 @@
+function Add(left, right) {
+    return left + right;
+}
+console.info(Add(1, 2));
---
+++ typescriptPractice/runoob/1.ts
@@ -0,0 +1,5 @@
+function Add(left: number, right: number): number {
+    return left + right;
+}
+
+console.info(Add(1, 2));
