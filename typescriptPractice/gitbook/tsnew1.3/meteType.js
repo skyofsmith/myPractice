a9008375eaeb99830dbd1a925dd9ccf8f7386160
drzz <skyofsmith@163.com>
2018-09-15 11:55:01 +0800

    meteType

---
+++ typescriptPractice/gitbook/tsnew1.3/meteType.js
@@ -0,0 +1,10 @@
+var x;
+x = ['hello', 10];
+console.log(x[0].substr(1));
+x[3] = 'world';
+console.log(x[5].toString());
+/*
+x = [10, 'hello'];
+console.log(x[1].substr(1));
+x[6] = true;
+*/ 
--- typescriptPractice/gitbook/tsnew1.3/meteType.ts
+++ typescriptPractice/gitbook/tsnew1.3/meteType.ts
@@ -1,13 +1,12 @@
-// Declare a tuple type
 var x: [string, number];
-// 初始化
-x = ['hello', 10]; // OK
-// 错误的初始化
-x = [10, 'hello']; // Error
+x = ['hello', 10]; 
 
-console.log(x[0].substr(1)); // OK
-console.log(x[1].substr(1)); // Error, 'number'没有'substr'方法
+console.log(x[0].substr(1));
+x[3] = 'world'; 
+console.log(x[5].toString()); 
 
-x[3] = 'world'; // OK
-console.log(x[5].toString()); // OK, 'string'和'number'都有toString
-x[6] = true; // Error, boolean不是number或string
+/*
+x = [10, 'hello'];
+console.log(x[1].substr(1));
+x[6] = true;
+*/
