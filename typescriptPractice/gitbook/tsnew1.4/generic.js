5c4a1717d0bb1b6856b32fb019581c6f97c34932
drzz <skyofsmith@163.com>
2018-09-17 23:15:04 +0800

    generic

---
+++ typescriptPractice/gitbook/tsnew1.4/generic.js
@@ -0,0 +1,4 @@
+function equal(lhs, rhs) {
+    return lhs === rhs;
+}
+// var e = equal(42, 'hello');
--- typescriptPractice/gitbook/tsnew1.4/generic.ts
+++ typescriptPractice/gitbook/tsnew1.4/generic.ts
@@ -1,7 +1,5 @@
 function equal<T>(lhs: T, rhs: T): boolean {
     return lhs === rhs;
-  }
-  
-  // 之前没有错误
-  // 现在会报错：在string和number之前没有最佳的基本类型
-  var e = equal(42, 'hello');
+}
+ 
+// var e = equal(42, 'hello');
