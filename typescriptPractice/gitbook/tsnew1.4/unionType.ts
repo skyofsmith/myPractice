091ac899517f0679b9ed3ed5b93e217e3b47226d
drzz <skyofsmith@163.com>
2018-09-17 23:05:44 +0800

    unionType .length

--- typescriptPractice/gitbook/tsnew1.4/unionType.ts
+++ typescriptPractice/gitbook/tsnew1.4/unionType.ts
@@ -7,3 +7,8 @@ var opts: RunOptions;
 opts.commandline = '-hello world';
 opts.commandline = ['-hello', 'world'];
 // opts.commandline = [42];
+
+
+if(opts.length === 0) { // OK, string和string[]都有'length'属性
+  console.log("it's empty");
+}
