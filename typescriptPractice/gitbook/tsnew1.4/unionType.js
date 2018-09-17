4c0f8f5e21a956cce1f74b287db52193805f06c9
drzz <skyofsmith@163.com>
2018-09-17 23:13:48 +0800

    tsc unionType.ts

--- typescriptPractice/gitbook/tsnew1.4/unionType.js
+++ typescriptPractice/gitbook/tsnew1.4/unionType.js
@@ -2,7 +2,7 @@ var opts;
 opts.commandline = '-hello world';
 opts.commandline = ['-hello', 'world'];
 // opts.commandline = [42];
-if (opts.length === 0) { // OK, string和string[]都有'length'属性
+if (opts.commandline.length === 0) { // OK, string和string[]都有'length'属性
     console.log("it's empty");
 }
 function formatCommandline(c) {
