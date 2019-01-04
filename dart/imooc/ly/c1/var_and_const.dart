2e8ae2f967387261f5c20b7194a9ba68815e3c0f
drzz <skyofsmith@163.com>
2019-01-04 21:05:02 +0800

    dart begin

---
+++ dart/imooc/ly/c1/var_and_const.dart
@@ -0,0 +1,21 @@
+void main(List<String> args) {
+  var a;
+  print(a);
+
+  a = 10;
+  print(a);
+
+  a = 'Hello Dart!';
+  print(a);
+
+  var b = 20;
+  print(b);
+
+  final c = 30;
+  print(c);
+  // c = 1;
+
+  const d = 11;
+  print(d);
+  // d = 22;
+}
