ff6c83a92b162c03f3dc4fc45faf60228aa5f13e
drzz <skyofsmith@163.com>
2019-03-30 22:33:42 +0800

    fix rxjs

--- requirejsPratice/rxjs/01/index.js
+++ requirejsPratice/rxjs/01/index.js
@@ -1,10 +1,6 @@
-import * as rxjs from 'rxjs'
+// import * as rxjs from 'rxjs'
 
 console.log(rxjs);
 const { fromEvent } = rxjs;
 
 rxjs.of(1, 2, 3)
-
-// const button = document.querySelector('button');
-// fromEvent(button, 'click')
-//   .subscribe(() => console.log('Clicked!'));
