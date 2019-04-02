ba8bae6af425ac56baf4e80d125e4ea94802b735
drzz <skyofsmith@163.com>
2019-04-02 23:27:38 +0800

    learn rxjs.

--- requirejsPratice/rxjs/01/index.js
+++ requirejsPratice/rxjs/01/index.js
@@ -1,13 +1,16 @@
-// import * as rxjs from 'rxjs'
-window.Rx = rxjs
+import * as rxjs from 'rxjs'
+import Rx from 'rxjs/Rx';
+
 console.log(rxjs)
 const { fromEvent } = rxjs
 
 rxjs.of(1, 2, 3)
+
+console.log(Rx);
 
 // use
 var button = document.querySelector('button#btn1')
 button.addEventListener('click', () => console.log('Clicked!'))
 
 var button = document.querySelector('button#btn2')
-rxjs.fromEvent(button, 'click').subscrible(() => console.log('Clicked!'))
+Rx.Observable.fromEvent(button, 'click').subscrible(() => console.log('Clicked!'))
