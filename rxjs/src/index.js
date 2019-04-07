382fa8a89c82a94968acd534b75a75cffa53eac4
drzz <skyofsmith@163.com>
2019-04-07 23:15:33 +0800

    Purity of rxjs

--- rxjs/src/index.js
+++ rxjs/src/index.js
@@ -1,5 +1,6 @@
 import * as rxjs from 'rxjs'
 import { Observable, fromEvent } from 'rxjs'
+import { scan } from 'rxjs/operators'
 
 console.log(rxjs)
 
@@ -9,10 +10,13 @@ btn1.addEventListener('click', function() {
 })
 
 var btn2 = document.getElementById('btn2')
-fromEvent(btn2, 'click')
-  .subscribe(() => console.log('Clicked!'))
+fromEvent(btn2, 'click').subscribe(() => console.log('Clicked!'))
 
-var count = 0;
-var btn3 = document.getElementById('btn3');
-btn3.addEventListener('click', () => console.log(`Clicked ${++count} times`));
+var count = 0
+var btn3 = document.getElementById('btn3')
+btn3.addEventListener('click', () => console.log(`Clicked ${++count} times`))
 
+const btn4 = document.getElementById('btn4')
+fromEvent(btn4, 'click')
+  .pipe(scan(count => count + 1, 0))
+  .subscribe(count => console.log(`Clicked ${count} times`))
