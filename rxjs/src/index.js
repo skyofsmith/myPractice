3f912837f5e6ab059427bab4ff2633cdd6e3249e
drzz <skyofsmith@163.com>
2019-04-07 23:02:52 +0800

    rxjs goon

--- rxjs/src/index.js
+++ rxjs/src/index.js
@@ -11,3 +11,8 @@ btn1.addEventListener('click', function() {
 var btn2 = document.getElementById('btn2')
 fromEvent(btn2, 'click')
   .subscribe(() => console.log('Clicked!'))
+
+var count = 0;
+var btn3 = document.getElementById('btn3');
+btn3.addEventListener('click', () => console.log(`Clicked ${++count} times`));
+
