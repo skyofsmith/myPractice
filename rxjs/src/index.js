30ad4ce64576d9ddacd4ac554232ee1f74c04433
drzz <skyofsmith@163.com>
2019-04-07 23:18:29 +0800

    Flow of rxjs

--- rxjs/src/index.js
+++ rxjs/src/index.js
@@ -12,6 +12,7 @@ btn1.addEventListener('click', function() {
 var btn2 = document.getElementById('btn2')
 fromEvent(btn2, 'click').subscribe(() => console.log('Clicked!'))
 
+// 纯净性 (Purity)
 var count = 0
 var btn3 = document.getElementById('btn3')
 btn3.addEventListener('click', () => console.log(`Clicked ${++count} times`))
@@ -19,4 +20,23 @@ btn3.addEventListener('click', () => console.log(`Clicked ${++count} times`))
 const btn4 = document.getElementById('btn4')
 fromEvent(btn4, 'click')
   .pipe(scan(count => count + 1, 0))
+  .subscribe(count => console.log(`Clicked ${count} times`))
+
+// 流动性 (Flow)
+
+var count = 0
+var rate = 1000
+var lastClick = Date.now() - rate
+var btn5 = document.getElementById('btn5')
+btn5.addEventListener('click', () => {
+  if (Date.now() - lastClick >= rate) {
+    console.log(`Clicked ${++count} times`)
+    lastClick = Date.now()
+  }
+})
+
+var btn6 = document.getElementById('btn6')
+fromEvent(btn6, 'click')
+  .throttleTime(1000)
+  .scan(count => count + 1, 0)
   .subscribe(count => console.log(`Clicked ${count} times`))
