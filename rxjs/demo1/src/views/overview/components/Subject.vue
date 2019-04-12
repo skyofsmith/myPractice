eba52aafebf5a0882155cdda2e5b3e1c6bf868d6
drzz <skyofsmith@163.com>
2019-04-12 23:27:45 +0800

    subject learn some.

--- rxjs/demo1/src/views/overview/components/Subject.vue
+++ rxjs/demo1/src/views/overview/components/Subject.vue
@@ -1,11 +1,46 @@
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+.subject {
+
+}
+</style>
 <template>
-  <div>
+  <div class="subject">
     Subject
   </div>
 </template>
 <script>
+import { Subject, from } from "rxjs";
+
 export default {
-  name: "Subject"
+  name: "Subject",
+  mounted() {
+    {
+      const subject = new Subject /*<number>*/();
+
+      subject.subscribe({
+        next: v => console.log(`observerA: ${v}`)
+      });
+      subject.subscribe({
+        next: v => console.log(`observerB: ${v}`)
+      });
+
+      subject.next(1);
+      subject.next(2);
+    }
+    {
+      const subject = new Subject /*<number>*/();
+
+      subject.subscribe({
+        next: v => console.log(`observerA: ${v}`)
+      });
+      subject.subscribe({
+        next: v => console.log(`observerB: ${v}`)
+      });
+
+      const observable = from([1, 2, 3]);
+
+      observable.subscribe(subject);
+    }
+  }
 };
 </script>
