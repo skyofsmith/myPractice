29495c5db42051b24c9194eabcb66a554d207e40
drzz <skyofsmith@163.com>
2019-05-08 22:30:24 +0800

    koa: app.callback

--- koa/demo0/index.js
+++ koa/demo0/index.js
@@ -1,8 +1,32 @@
 const Koa = require('koa')
 const app = new Koa()
 
+
+// logger
+
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// response
+
 app.use(async ctx => {
   ctx.body = 'hello world'
 })
 
+console.log('app.env is', app.env);
+
 app.listen(3000)
+// up = down
+// require('http').createServer(app.callback()).listen(3000)
