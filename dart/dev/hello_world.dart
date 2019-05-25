426a318c416ef0b534f431a5d32483dcc7741485
drzz <skyofsmith@163.com>
2019-05-25 17:36:44 +0800

    dart sample

---
+++ dart/dev/hello_world.dart
@@ -0,0 +1,105 @@
+// Imports
+
+// Importing core libraries
+import 'dart:math';
+
+// Importing libraries from external packages
+//import 'package:test/test.dart';
+
+// Importing files
+//import 'path/to/my_other_file.dart';
+
+// Classes
+class Spacecraft {
+  String name;
+  DateTime launchDate;
+
+  // Constructor, with syntactic sugar for assignment to members.
+  Spacecraft(this.name, this.launchDate) {
+    // Initialization code goes here.
+  }
+
+  // Named constructor that forwards to the default one.
+  Spacecraft.unlaunched(String name) : this(name, null);
+
+  int get launchYear =>
+      launchDate?.year; // read-only non-final property
+
+  // Method.
+  void describe() {
+    print('Spacecraft: $name');
+    if (launchDate != null) {
+      int years =
+          DateTime.now().difference(launchDate).inDays ~/
+              365;
+      print('Launched: $launchYear ($years years ago)');
+    } else {
+      print('Unlaunched');
+    }
+  }
+}
+
+void main() {
+  // main function
+  print('Hello, World!');
+
+  // variable
+  var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
+  print(name);
+  print(antennaDiameter);
+  print(image);
+
+  // flow statement
+  if (year >= 2001) {
+    print('21st century');
+  } else if (year >= 1901) {
+    print('20th century');
+  }
+
+  for (var object in flybyObjects) {
+    print(object);
+  }
+
+  for (int month = 1; month <= 12; month++) {
+    print(month);
+  }
+
+  while (year < 2016) {
+    year += 1;
+  }
+
+  // Functions
+  int fibonacci(int n) {
+    if (n == 0 || n == 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+
+  var result = fibonacci(20);
+  print(result);
+
+  flybyObjects.where((name) => name.contains('turn')).forEach(print);
+
+  // Comments
+
+  // This is a normal, one-line comment.
+
+  /// This is a documentation comment, used to document libraries,
+  /// classes, and their members. Tools like IDEs and dartdoc treat
+  /// doc comments specially.
+
+  /* Comments like these are also supported. */
+
+  // class
+  var voyager = Spacecraft('Voyager I', DateTime(1977, 9, 5));
+  voyager.describe();
+
+  var voyager3 = Spacecraft.unlaunched('Voyager III');
+  voyager3.describe();
+}
