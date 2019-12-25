9a53787001361f762e3d0752ebda9df32a30d9b0
skyofsmith <skyofsmith@163.com>
2019-12-25 22:04:58 +0800

    2optional_parameter.dart

--- dart/imooc/ly/05method/2optional_parameter.dart
+++ dart/imooc/ly/05method/2optional_parameter.dart
@@ -2,8 +2,18 @@
 
 void main() {
   printPerson("sam");
+  printPerson("Drzz", age: 28, gender: "Male");
+  printPerson("Drzz", gender: "Male");
+
+  printPerson2("sam");
+  printPerson2("Drzz", 18);
+  printPerson2("Drzz", 18, "Male");
+//  printPerson2("Drzz", "Male");
 }
 
 printPerson(String name, {int age, String gender}) {
+  print("name=$name,age=$age,gender=$gender");
+}
+printPerson2(String name, [int age, String gender]) {
   print("name=$name,age=$age,gender=$gender");
 }
