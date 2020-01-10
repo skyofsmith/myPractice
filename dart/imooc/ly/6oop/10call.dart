86eec8cd4aa315e0875d1912d82b18d5670f978e
skyofsmith <skyofsmith@163.com>
2020-01-10 22:53:45 +0800

    10call.dart

--- dart/imooc/ly/6oop/10call.dart
+++ dart/imooc/ly/6oop/10call.dart
@@ -2,15 +2,21 @@ void main() {
   var person = new Person();
   person.name = "Tom";
   person.age = 20;
-
-  person.work();
+//
+//  person.work();
+//  person();
+  print(person("Sam", 28));
 }
 
 class Person {
   String name;
   int age;
 
-  void work() {
-    print("Name is $name, Age is $age");
+  String work() {
+    return ("Name is $name, Age is $age");
+  }
+
+  String call(String name, int age) {
+    return ("Name is $name, Age is $age");
   }
 }
