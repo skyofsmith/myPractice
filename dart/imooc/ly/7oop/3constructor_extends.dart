20b6c8476b5f3ec96fb6b13239f514cd64b8a2db
skyofsmith <skyofsmith@163.com>
2020-01-13 22:58:56 +0800

    3constructor_extends.dart

--- dart/imooc/ly/7oop/3constructor_extends.dart
+++ dart/imooc/ly/7oop/3constructor_extends.dart
@@ -1,15 +1,18 @@
 void main() {
-  var s = new Student();
+  var s = new Student("Sam");
+  print(s.name);
 }
 
-class Person{
+class Person {
   String name;
 
-  Person() {
-    print("Person...");
-  }
+  Person(this.name);
+
+  Person.withName(this.name);
 }
 
 class Student extends Person {
   int age;
+
+  Student(String name) :super.withName(name);
 }
