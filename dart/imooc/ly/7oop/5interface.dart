e9a481afd844429c1c90eca64dec05a0e635e50c
skyofsmith <skyofsmith@163.com>
2020-01-16 22:55:17 +0800

    5interface.dart

--- dart/imooc/ly/7oop/5interface.dart
+++ dart/imooc/ly/7oop/5interface.dart
@@ -1,22 +1,13 @@
 void main() {
-  var p = new Person();
-  p.run();
   var s = new Student();
   s.run();
 }
-class Person {
-  String name;
-  int get age => 18;
+abstract class Person {
 
-  void run() {
-    print("Person run..");
-  }
+  void run();
 }
 
 class Student implements Person {
-  String name;
-
-  int get age => null;
 
   void run() {
     print("Student run..");
