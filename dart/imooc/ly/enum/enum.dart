3b153b19b43201a772ea2bed4a653903621afe38
skyofsmith <skyofsmith@163.com>
2020-01-20 22:58:44 +0800

    enum.dart

--- dart/imooc/ly/enum/enum.dart
+++ dart/imooc/ly/enum/enum.dart
@@ -1,5 +1,20 @@
 void main() {
+  var currentSeason = Season.winter;
 
+  switch(currentSeason) {
+    case Season.spring:
+      print("1~3");
+      break;
+    case Season.summer:
+      print("4~6");
+      break;
+    case Season.autumn:
+      print("7~9");
+      break;
+    case Season.winter:
+      print("10~12");
+      break;
+  }
 }
 
 enum Season {
