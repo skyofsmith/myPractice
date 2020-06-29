afc64c3d63dbcd5f5a1b4d44aaa99f5ac12aedfe
skyofsmith <skyofsmith@163.com>
2020-06-30 00:24:11 +0800

    snowpack demo of a vue3

---
+++ snowpack/demo/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="App">
+    count is {{ state.count }}
+    <br>
+    double is {{ state.double }}
+    <br>
+    <button @click="add">+</button>
+  </div>
+</template>
+
+<script>
+import { reactive, computed } from 'vue'
+export default {
+  setup() {
+    const state = reactive({
+      count: 0,
+      double: computed(() => (state.count * 2))
+    })
+    function add() {
+      state.count++
+
+    }
+    return {
+      state,
+      add
+    };
+  }
+};
+</script>
+
+<style>
+.App {
+  text-align: center;
+}
+.App-header {
+  background-color: #f9f6f6;
+  color: #32485f;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+}
+.App-link {
+  color: #00c185;
+}
+.App-logo {
+  height: 40vmin;
+  pointer-events: none;
+  margin-bottom: 1rem;
+  animation: App-logo-spin infinite 1.6s ease-in-out alternate;
+}
+@keyframes App-logo-spin {
+  from {
+    transform: scale(1);
+  }
+  to {
+    transform: scale(1.06);
+  }
+}
+</style>
