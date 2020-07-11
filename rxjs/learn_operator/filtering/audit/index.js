503064ada8e553017143fa87346483956ac20377
skyofsmith <skyofsmith@163.com>
2020-07-11 22:09:51 +0800

    audit auditTime

--- rxjs/learn_operator/filtering/audit/index.js
+++ rxjs/learn_operator/filtering/audit/index.js
@@ -1,15 +1,20 @@
 import {clickAndPrint} from '../../utils/events'
 import { fromEvent, interval } from 'rxjs';
-import { audit } from 'rxjs/operators'
+import { audit, tap } from 'rxjs/operators'
 
 clickAndPrint('#btn1', '#demo1', print => {
+  const clickTime = Date.now();
+  let count = 0;
   const clicks = fromEvent(document, 'click');
-  const result = clicks.pipe(audit(x => {
-    print(`${Date.now()}: ${x.clientX},${x.clientY}.clicked`);
+  const result = clicks.pipe(
+    tap(x => {
+      count++
+      print(`${count}...${Date.now() - clickTime}: (${x.clientX},${x.clientY}).clicked`);
+    }),
+    audit(x => {
     return interval(1000)
   }));
   result.subscribe(x => {
-
-    print(`${Date.now()}: ${x.clientX},${x.clientY}.received`)
+    print(`${count}...${Date.now() - clickTime}: (${x.clientX},${x.clientY}).received`)
   });
 });
--- rxjs/learn_operator/filtering/auditTime/index.js
+++ rxjs/learn_operator/filtering/auditTime/index.js
@@ -1,1 +1,18 @@
 import {clickAndPrint} from '../../utils/events'
+import {fromEvent, interval} from 'rxjs';
+import {audit, auditTime, tap} from 'rxjs/operators';
+
+clickAndPrint('#btn1', '#demo1', print => {
+  const clickTime = Date.now();
+  let count = 0;
+  const clicks = fromEvent(document, 'click');
+  const result = clicks.pipe(
+    tap(x => {
+      count++;
+      print(`${count}...${Date.now() - clickTime}: (${x.clientX},${x.clientY}).clicked`);
+    }),
+    auditTime(1000));
+  result.subscribe(x => {
+    print(`${count}...${Date.now() - clickTime}: (${x.clientX},${x.clientY}).received`)
+  });
+});
