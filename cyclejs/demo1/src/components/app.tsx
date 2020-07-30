fb836c74c79656097076db3315df7b32719e053f
skyofsmith <skyofsmith@163.com>
2020-07-30 23:08:40 +0800

    ..

--- cyclejs/demo1/src/components/app.tsx
+++ cyclejs/demo1/src/components/app.tsx
@@ -1,24 +1,24 @@
-import xs, { Stream } from 'xstream';
-import { VNode, DOMSource } from '@cycle/dom';
-import { extractSinks } from 'cyclejs-utils';
-import isolate from '@cycle/isolate';
+import xs, { Stream } from 'xstream'
+import { VNode, DOMSource } from '@cycle/dom'
+import { extractSinks } from 'cyclejs-utils'
+import isolate from '@cycle/isolate'
 
-import { driverNames } from '../drivers';
-import { Sources, Sinks, Reducer, Component } from '../interfaces';
+import { driverNames } from '../drivers'
+import { Sources, Sinks, Reducer, Component } from '../interfaces'
 
-import { Counter, State as CounterState } from './counter';
-import { Speaker, State as SpeakerState } from './speaker';
+import { Counter, State as CounterState } from './counter'
+import { Speaker, State as SpeakerState } from './speaker'
 
 export interface State {
-    counter?: CounterState;
-    speaker?: SpeakerState;
+    counter?: CounterState
+    speaker?: SpeakerState
 }
 
 export function App(sources: Sources<State>): Sinks<State> {
     const match$ = sources.router.define({
         '/counter': isolate(Counter, 'counter'),
         '/speaker': isolate(Speaker, 'speaker')
-    });
+    })
 
     const componentSinks$: Stream<Sinks<State>> = match$
         .filter(({ path, value }: any) => path && typeof value === 'function')
@@ -26,16 +26,16 @@ export function App(sources: Sources<State>): Sinks<State> {
             return value({
                 ...sources,
                 router: sources.router.path(path)
-            });
-        });
+            })
+        })
 
     const redirect$: Stream<string> = sources.router.history$
         .filter((l: Location) => l.pathname === '/')
-        .mapTo('/counter');
+        .mapTo('/counter')
 
-    const sinks = extractSinks(componentSinks$, driverNames);
+    const sinks = extractSinks(componentSinks$, driverNames)
     return {
         ...sinks,
         router: xs.merge(redirect$, sinks.router)
-    };
+    }
 }
