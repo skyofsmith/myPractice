f71c0575423145491de0dd55e163a355a0a06b76
skyofsmith <skyofsmith@163.com>
2020-08-05 23:07:41 +0800

    learn cycle

--- cyclejs/cycle-xstream/p1/index.js
+++ cyclejs/cycle-xstream/p1/index.js
@@ -1,2 +1,11 @@
-window.xs = xstream
-xs.periodic(1000) // -
+window.xs = xstream.default;
+xs.periodic(1000)
+  .fold(prev => prev + 1, 0)
+  .map(i=>`Secondselapsed:${i}`)
+
+  .subscribe({
+    next: str => {
+      const elem = document.querySelector('#app');
+      elem.textContent = str
+    }
+  })
