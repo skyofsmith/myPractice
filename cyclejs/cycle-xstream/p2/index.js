e0cc5fe52b979a684e2bc80ddeb7beeae65bf5e4
skyofsmith <skyofsmith@163.com>
2020-08-06 22:23:41 +0800

    learn cycle use xstream.

---
+++ cyclejs/cycle-xstream/p2/index.js
@@ -0,0 +1,29 @@
+window.xs = xstream.default;
+
+function main() {
+  return xs.periodic(1000)
+    .fold(prev => prev + 1, 0)
+    .map(i => `Secondselapsed:${i}`);
+}
+
+
+function domDriver(text$) {
+  text$.subscribe({
+    next: str => {
+      const elem = document.querySelector('#app');
+      elem.textContent = str
+    }
+  });
+}
+
+function logDriver(msg$) {
+  msg$.subscribe({
+    next: msg => {
+      console.log(msg)
+    }
+  })
+}
+
+const sink = main();
+domDriver(sink);
+logDriver(sink);
