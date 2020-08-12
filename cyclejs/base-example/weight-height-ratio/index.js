050e2ad85f27f38dcbaca220cd9d8aeca027647b
skyofsmith <skyofsmith@163.com>
2020-08-12 17:17:30 +0800

    rebuild main

--- cyclejs/base-example/weight-height-ratio/index.js
+++ cyclejs/base-example/weight-height-ratio/index.js
@@ -1,58 +1,59 @@
-import xs from 'xstream';
-import {run} from '@cycle/run';
-import {div, input, h2, makeDOMDriver} from '@cycle/dom';
+import xs from 'xstream'
+import { run } from '@cycle/run'
+import { div, input, h2, makeDOMDriver } from '@cycle/dom'
 
-    function renderWeightSlider(weight) {
-        return div([
-            'Weight ' + weight + 'kg',
-            input('.weight', {type: 'range', min: 40, max: 140, value: weight})
-        ]);
-    }
-    
-    function renderHeightSlider(height) {
-        return div([
-            'Height ' + height + 'cm',
-            input('.height', {type: 'range', min: 140, max: 210, value: height})
-        ]);
-    }
-    
-    function bmi(weight, height) {
-        const heightMeters = height * 0.01;
-        return Math.round(weight / (heightMeters * heightMeters));
-    }
-    function main(sources) {
-    
-    const changeWeight$ = sources.DOM.select('.weight')
-        .events('input')
-        .map(ev => ev.target.value);
+function renderWeightSlider(weight) {
+  return div([
+    'Weight ' + weight + 'kg',
+    input('.weight', { type: 'range', min: 40, max: 140, value: weight }),
+  ])
+}
 
-    const changeHeight$ = sources.DOM.select('.height')
-        .events('input')
-        .map(ev => ev.target.value);
+function renderHeightSlider(height) {
+  return div([
+    'Height ' + height + 'cm',
+    input('.height', { type: 'range', min: 140, max: 210, value: height }),
+  ])
+}
 
-    const weight$ = changeWeight$.startWith(70);
-    const height$ = changeHeight$.startWith(170);
+function bmi(weight, height) {
+  const heightMeters = height * 0.01
+  return Math.round(weight / (heightMeters * heightMeters))
+}
 
-    const state$ = xs.combine(weight$, height$)
-        .map(([weight, height]) => {
-            const heightMeters = height * 0.01;
-            const bmi = Math.round(weight / (heightMeters * heightMeters));
-            return {weight, height, bmi: bmi(weight, height)};
-        });
+function view(state$) {
+  return state$.map(({ weight, height, bmi }) =>
+    div([
+      renderWeightSlider(weight),
+      renderHeightSlider(height),
+      h2('BMI is ' + bmi),
+    ])
+  )
+}
 
-    const vdom$ = state$.map(({weight, height, bmi}) =>
-        div([
-            renderWeightSlider(weight),
-            renderHeightSlider(height),
-            h2('BMI is ' + bmi)
-        ])
-    );
+function main(sources) {
+  const changeWeight$ = sources.DOM.select('.weight')
+    .events('input')
+    .map((ev) => ev.target.value)
 
-    return {
-        DOM: vdom$
-    };
+  const changeHeight$ = sources.DOM.select('.height')
+    .events('input')
+    .map((ev) => ev.target.value)
+
+  const weight$ = changeWeight$.startWith(70)
+  const height$ = changeHeight$.startWith(170)
+
+  const state$ = xs.combine(weight$, height$).map(([weight, height]) => {
+    return { weight, height, bmi: bmi(weight, height) }
+  })
+
+  const vdom$ = view(state$)
+
+  return {
+    DOM: vdom$,
+  }
 }
 
 run(main, {
-    DOM: makeDOMDriver('#app')
-});
+  DOM: makeDOMDriver('#app'),
+})
