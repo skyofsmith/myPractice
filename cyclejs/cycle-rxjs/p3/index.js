3a89c8b85e63f250ad75a2931d5701380ad0f0aa
skyofsmith <skyofsmith@163.com>
2020-08-13 17:01:36 +0800

    p3

---
+++ cyclejs/cycle-rxjs/p3/index.js
@@ -0,0 +1,23 @@
+// Logic(functional)
+function main() {
+  return {
+    DOM: Rx.Observable.timer(0, 1000).map((i) => `Seconds elapsed ${i}`),
+    Log: Rx.Observable.timer(0, 2000).map(i =>  2 *i)
+  };
+}
+
+// Effects(imperative)
+function DOMEffect(text$) {
+  text$.subscribe((text) => {
+    const container = document.querySelector('#app')
+    container.textContent = text
+  })
+}
+
+function consoleLogEffect(msg$) {
+  msg$.subscribe((msg) => console.log(msg))
+}
+
+const sink = main();
+DOMEffect(sink.DOM);
+consoleLogEffect(sink.Log);
